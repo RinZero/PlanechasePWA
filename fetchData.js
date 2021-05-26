a1cb5710d043057a62a72fd6ceed3d09fed9d4c5
Jonathan <jonathan@lex.or.at>
2021-05-26 02:18:07 +0200

    initial commit

---
+++ fetchData.js
@@ -0,0 +1,55 @@
+import 'regenerator-runtime/runtime';
+import axios from 'axios';
+
+const getCards = async () => {
+  const cards = await axios.get('https://api.scryfall.com/cards/search?q=t%3Aplane+or+t%3Aphenom')
+  .then(function (response) {
+      // handle success
+      return response.data.data
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+  return cards
+}
+
+const shuffleDeck = (deck) => {
+  let currentIndex = deck.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = deck[currentIndex];
+    deck[currentIndex] = deck[randomIndex];
+    deck[randomIndex] = temporaryValue;
+  }
+  return deck;
+}
+
+ const testFunc = async() => {
+  const deck = await getCards()
+  await shuffleDeck(deck)
+  console.log(deck)
+}
+
+
+const butNewGame = document.getElementById("butNewGame");
+const butSend = document.getElementById("butSend");
+
+let defferredPrompt;
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(reg => {
+        console.log("Service worker registered!", reg);
+      })
+      .catch(e => {
+        console.log("Error!", e);
+      });
+  });
+}
+
+butNewGame.addEventListener("click", () => {
+  testFunc()
+});
---
+++ sw.js
@@ -0,0 +1,52 @@
+
+// import 'regenerator-runtime/runtime';
+// import axios from 'axios';
+
+
+// export const getCards = async () => {
+//     const cards = await axios.get('https://api.scryfall.com/cards/search?q=type%3Aplane')
+//     .then(function (response) {
+//         // handle success
+//         console.log(response.data.data)
+//         return response.data.data
+//       })
+//       .catch(function (error) {
+//         // handle error
+//         console.log(error);
+//       })
+//     return cards
+//   }
+  
+//   export const shuffleDeck = (deck) => {
+//     let currentIndex = deck.length, temporaryValue, randomIndex;
+//     while (0 !== currentIndex) {
+//       randomIndex = Math.floor(Math.random() * currentIndex);
+//       currentIndex -= 1;
+//       temporaryValue = deck[currentIndex];
+//       deck[currentIndex] = deck[randomIndex];
+//       deck[randomIndex] = temporaryValue;
+//     }
+//     return deck;
+//   }
+const SW_VERSION = 1;
+const CACHE_NAME = `OFFLINE_VERSION_${SW_VERSION}`;
+const OFFLINE_URL = "offline.html";
+
+
+self.addEventListener("install", (event) => {
+  console.log("[ServiceWorker] install event", event);
+  //self.skipWaiting();
+
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+
+      await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
+    //   const deck = await getCards()
+    //   console.log(deck)
+    //   //await shuffleDeck(deck)
+    //   await cache.add(new Request(deck, { cache: "reload"}))
+      console.log("Offline page cached");
+    })()
+  );
+});
