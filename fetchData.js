1c9e58cbd3cc0f966270016b6ba088684fd657df
Jonathan <jonathan@lex.or.at>
2021-05-27 02:00:49 +0200

    extend functionallity

--- fetchData.js
+++ fetchData.js
@@ -32,17 +32,138 @@ const shuffleDeck = (deck) => {
   console.log(deck)
 }
 
+const askPermission = () => {
+  return new Promise(function(resolve, reject) {
+    const permissionResult = Notification.requestPermission(function(result) {
+      resolve(result);
+    });
+
+    if (permissionResult) {
+      permissionResult.then(resolve, reject);
+    }
+  })
+  .then(function(permissionResult) {
+    if (permissionResult !== 'granted') {
+      throw new Error('We weren\'t granted permission.');
+    }
+  });
+}
+
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
 
 const butNewGame = document.getElementById("butNewGame");
+const butInstall = document.getElementById("butInstall");
 const butSend = document.getElementById("butSend");
+const subscriptionField = document.getElementById('subscriptionField');
+const butUnsubscribe = document.getElementById("butUnsubscribe");
+const butRollDice = document.getElementById("butRollDice");
+let swRegistration = null;
+
+function subscribeUser() {
+  const applicationServerKey = urlB64ToUint8Array('BIQ8OhowXfTajbqmQIiX7YXLiG4sPaIAfKU76M5zDHZte7x0HLwZrm5_rh3mSPLE7BTz-fuaBNgZ-NEP9944lUo')
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: applicationServerKey
+  })
+  .then(function(subscription) {
+    console.log('User is subscribed', subscription);
+
+    //updateSubscriptionOnServer(subscription);
+    // refresh ui and send Subscription to server
+    subscriptionField.classList.remove('is-invisible')
+    subscriptionField.textContent = JSON.stringify(subscription);
+    isSubscribed = true;
+
+  })
+  .catch(function(err) {
+    console.log('Failed to subscribe the user: ', err);
+  });
+}
+
+const unsubscribeUser = () => {
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    if (subscription) {
+      return subscription.unsubscribe();
+    }
+  })
+  .catch(function(error) {
+    console.log('Error unsubscribing', error);
+  })
+  .then(function() {
+    updateSubscriptionOnServer(null);
+
+    console.log('User is unsubscribed.');
+    isSubscribed = false;
+
+    updateBtn();
+  });
+}
+
+
+
 
 let defferredPrompt;
-if ("serviceWorker" in navigator) {
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  console.log("Installation event fired");
+  e.preventDefault();
+
+  defferredPrompt = e;
+  return false;
+});
+
+butInstall.addEventListener("click", () => {
+  if (defferredPrompt) {
+    defferredPrompt.prompt();
+
+    defferredPrompt.userChoice.then((res) => {
+      if (res.outcome == "dismissed") {
+        console.log("Installation canceled");
+      } else {
+        console.log("App installed");
+      }
+      defferredPrompt = null;
+    });
+  }
+});
+
+
+if ("serviceWorker" in navigator && 'PushManager' in window) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
       .register("/sw.js")
       .then(reg => {
         console.log("Service worker registered!", reg);
+        swRegistration = reg;
+        subscribeUser()
+
+        navigator.serviceWorker.addEventListener('message', function(event){
+          console.log("test", event)
+          if(event.data){
+            
+            if( event.data.propery === "activeCard"&& event.data.state !== undefined){
+              setActiveCard(data.state, true)
+            }
+            
+            if( event.data.propery === "diceTax"&& event.data.state !== undefined){
+              setDiceTax(data.state, true)
+            }
+          }
+      });
       })
       .catch(e => {
         console.log("Error!", e);
@@ -51,5 +172,67 @@ if ("serviceWorker" in navigator) {
 }
 
 butNewGame.addEventListener("click", () => {
-  testFunc()
+  askPermission()
 });
+butUnsubscribe.addEventListener("click", () => {
+  unsubscribeUser()
+});
+butRollDice.addEventListener("click", () => {
+  let diceTax = getDiceTax()
+  setDiceTax(diceTax +2)
+})
+butSend.addEventListener("click", function () {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.controller.postMessage({
+      name: "Test",
+      surname: "Test",
+      title: "test",
+      number: 2
+    });
+  }
+});
+
+let refreshing;
+navigator.serviceWorker.addEventListener("controllerchange", () => {
+  if (refreshing) return;
+  refreshing = true;
+  window.location.reload(true);
+});
+
+
+const stateToServiceWorker =(data)=>{
+  if (navigator.serviceWorker && navigator.serviceWorker.controller) {
+      navigator.serviceWorker.controller.postMessage(data);
+  }
+}
+const getDiceTax = () => {
+  let diceTax = parseInt(document.getElementById("diceTax").innerHTML);
+  if (!isNaN(diceTax)) {
+      return diceTax;
+  } else {
+      return 0;
+  }
+}
+
+const setDiceTax = (diceTax, fromOtherTab) => {
+  document.getElementById("diceTax").innerHTML = parseInt(diceTax);
+  if (!fromOtherTab) {
+      stateToServiceWorker({property: "diceTax", state: diceTax});
+  }
+}
+
+const getActiveCard = () => {
+  let activeCard = parseInt(document.getElementById("activeCard").innerHTML);
+  if (!isNaN(activeCard)) {
+      return activeCard;
+  } else {
+      return 0;
+  }
+}
+
+const setActiveCard = (activeCard, fromOtherTab) => {
+  document.getElementById("activeCard").src = activeCard;
+  if (!fromOtherTab) {
+      stateToServiceWorker({property: "activeCard", state: activeCard});
+  }
+}
--- sw.js
+++ sw.js
@@ -29,9 +29,9 @@
 //   }
 const SW_VERSION = 1;
 const CACHE_NAME = `OFFLINE_VERSION_${SW_VERSION}`;
-const OFFLINE_URL = "offline.html";
+const OFFLINE_URL = "index.html";
 
-const assetPaths = ['assets/opca-1-chaotic-aether.jpg', 'assets/opca-2-interplanar-tunnel.jpg', 'assets/opca-3-morphic-tide.jpg']
+const assetPaths = ['assets/opca-1-chaotic-aether.jpg', 'assets/opca-2-interplanar-tunnel.jpg', 'assets/opca-3-morphic-tide.jpg', 'assets/opca-12-akoum.jpg']
 self.addEventListener("install", (event) => {
   console.log("[ServiceWorker] install event", event);
   //self.skipWaiting();
@@ -49,3 +49,101 @@ self.addEventListener("install", (event) => {
     })()
   );
 });
+
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+
+  const title = '1 Push Nachricht';
+  const options = {
+    body: 'Von Codelabs',
+    icon: 'assets/opca-2-interplanar-tunnel.jpg',
+    badge: 'assets/opca-2-interplanar-tunnel.jpg'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener("fetch",(fetchEvent) =>{
+  console.log("[Service Worker] Fetch", fetchEvent.request.url);
+
+  // self.clients.matchAll().then(function (clients) {
+  //   clients.forEach(function (client) {
+  //     client.postMessage(
+  //       "Hi client " + client.id + ". You requested:" + fetchEvent.request.url
+  //     );
+  //   });
+  // });
+
+  fetchEvent.respondWith(
+    (async () => {
+      try {
+        const networkResponse = await fetch(fetchEvent.request);
+        return networkResponse;
+      } catch (error) {
+        console.log(
+          "Fetch failed! returning cached page instead: ",
+          error
+        );
+
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(OFFLINE_URL);
+
+        return cachedResponse;
+      }
+    })()
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  //return self.clients.claim();
+});
+
+// self.addEventListener("message", function (event) {
+//   console.log("Service worker received message:", event.data);
+// });
+
+self.addEventListener('message', function(event){
+  console.log("hihihi")
+  // Receive the data from the client
+  var data = event.data;
+
+  // The unique ID of the tab
+  var clientId = event.source.id 
+
+  // A function that handles the message
+  self.syncTabState(data, clientId);
+});
+
+
+self.sendTabState = function(client, data){
+  // Post data to a specific client
+  client.postMessage(data);
+}
+
+
+self.syncTabState = function(data, clientId){
+  self.clients.matchAll().then(function(clients) {
+
+      // Loop over all available clients
+      clients.forEach(function(client) {
+
+          // No need to update the tab that 
+          // sent the data
+          if (client.id !== clientId) {
+              self.sendTabState(client, data)
+          }
+         
+      })
+  })
+}
